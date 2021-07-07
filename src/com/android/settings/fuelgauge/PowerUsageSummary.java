syberia-project/platform_packages_apps_Settings

SmartCharge: update for HAL implementation
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I5d0fc65aee115de7d179058dc0563482288fd75a

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -32,6 +32,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.os.SystemClock;
 import android.provider.Settings.Global;
 import android.text.format.Formatter;
@@ -65,7 +66,10 @@ import com.android.settingslib.utils.PowerUtil;
 import com.android.settingslib.utils.StringUtil;
 import com.android.settingslib.widget.LayoutPreference;
 
+import vendor.syberia.smartcharge.V1_0.ISmartCharge;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Displays a list of apps and subsystems that consume power, ordered by how much power was
@@ -86,6 +90,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_BATTERY_TEMP = "battery_temp";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
+
+    private ISmartCharge mSmartCharge = null;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -257,9 +263,21 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
         // Check availability of Smart Charging
         Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
-        if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
+        if (getSmartCharge() ==  null) {
             getPreferenceScreen().removePreference(mSmartCharging);
         }
+    }
+
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 
     @Override

src/com/android/settings/fuelgauge/smartcharging/SmartChargingSettings.java
@@ -18,6 +18,7 @@ package com.android.settings.fuelgauge.smartcharging;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -36,6 +37,9 @@ import com.syberia.settings.preference.CustomSeekBarPreference;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import vendor.syberia.smartcharge.V1_0.ISmartCharge;
 
 /**
  * Settings screen for Smart charging
@@ -48,17 +52,17 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
     private CustomSeekBarPreference mSmartChargingLevel;
     private CustomSeekBarPreference mSmartChargingResumeLevel;
 
-    private int mSmartChargingLevelDefaultConfig;
-    private int mSmartChargingResumeLevelDefaultConfig;
+    private ISmartCharge mSmartCharge;
+    private int mSmartChargingLevelDefaultConfig = 85;
+    private int mSmartChargingResumeLevelDefaultConfig = 80;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        mSmartChargingLevelDefaultConfig = getResources().getInteger(
-                com.android.internal.R.integer.config_smartChargingBatteryLevel);
-
-        mSmartChargingResumeLevelDefaultConfig = getResources().getInteger(
-                com.android.internal.R.integer.config_smartChargingBatteryResumeLevel);
-
+        mSmartCharge = getSmartCharge();
+        try {
+            mSmartChargingLevelDefaultConfig = mSmartCharge.getSuspendLevel();
+            mSmartChargingResumeLevelDefaultConfig = mSmartCharge.getResumeLevel();
+        } catch (RemoteException ex) { }
         mSmartChargingLevel = (CustomSeekBarPreference) findPreference(KEY_SMART_CHARGING_LEVEL);
         int currentLevel = Settings.System.getInt(getContentResolver(),
             Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
@@ -90,29 +94,49 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        int resumeLevel = Settings.System.getInt(getContentResolver(),
+                 Settings.System.SMART_CHARGING_RESUME_LEVEL, mSmartChargingResumeLevelDefaultConfig);
+        int suspendLevel = Settings.System.getInt(getContentResolver(),
+                 Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
+
         if (preference == mSmartChargingLevel) {
-            int smartChargingLevel = (Integer) objValue;
-            int mChargingResumeLevel = Settings.System.getInt(getContentResolver(),
-                     Settings.System.SMART_CHARGING_RESUME_LEVEL, mSmartChargingResumeLevelDefaultConfig);
+            suspendLevel = (Integer) objValue;
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_LEVEL, smartChargingLevel);
-                mSmartChargingResumeLevel.setMax(smartChargingLevel - 1);
-            if (smartChargingLevel <= mChargingResumeLevel) {
-                mSmartChargingResumeLevel.setValue(smartChargingLevel - 1);
+                    Settings.System.SMART_CHARGING_LEVEL, suspendLevel);
+            mSmartChargingResumeLevel.setMax(suspendLevel - 1);
+            if (suspendLevel <= resumeLevel) {
+                resumeLevel--;
+                mSmartChargingResumeLevel.setValue(suspendLevel -1);
                 Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_RESUME_LEVEL, smartChargingLevel - 1);
+                    Settings.System.SMART_CHARGING_RESUME_LEVEL, resumeLevel);
             }
+            try {
+                mSmartCharge.updateBatteryLevels(suspendLevel, resumeLevel);
+            } catch (RemoteException ex) { }
             return true;
         } else if (preference == mSmartChargingResumeLevel) {
-            int smartChargingResumeLevel = (Integer) objValue;
-            int mChargingLevel = Settings.System.getInt(getContentResolver(),
-                     Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
-                mSmartChargingResumeLevel.setMax(mChargingLevel - 1);
-               Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_RESUME_LEVEL, smartChargingResumeLevel);
+            resumeLevel = (Integer) objValue;
+            mSmartChargingResumeLevel.setMax(suspendLevel - 1);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.SMART_CHARGING_RESUME_LEVEL, resumeLevel);
+            try {
+                mSmartCharge.updateBatteryLevels(suspendLevel, resumeLevel);
+            } catch (RemoteException ex) { }
             return true;
         } else {
             return false;
         }
+    }
+
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 }

PixelExtended/packages_apps_Settings

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -26,6 +26,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.RemoteException;
 import android.provider.Settings.Global;
 import android.text.format.Formatter;
 import android.view.Menu;
@@ -55,7 +56,10 @@ import com.android.settingslib.utils.PowerUtil;
 import com.android.settingslib.utils.StringUtil;
 import com.android.settingslib.widget.LayoutPreference;
 
+import vendor.aosp.smartcharge.V1_0.ISmartCharge;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Displays a list of apps and subsystems that consume power, ordered by how much power was consumed
@@ -71,6 +75,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
     private static final String KEY_SCREEN_USAGE = "screen_usage";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
+
+    private ISmartCharge mSmartCharge = null;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -228,6 +234,18 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         }
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+    }
+  
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 
     @Override

src/com/android/settings/fuelgauge/smartcharging/SmartChargingSettings.java
@@ -18,6 +18,7 @@ package com.android.settings.fuelgauge.smartcharging;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -36,6 +37,9 @@ import org.aospextended.extensions.preference.CustomSeekBarPreference;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import vendor.aosp.smartcharge.V1_0.ISmartCharge;
 
 /**
  * Settings screen for Smart charging
@@ -48,17 +52,17 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
     private CustomSeekBarPreference mSmartChargingLevel;
     private CustomSeekBarPreference mSmartChargingResumeLevel;
 
-    private int mSmartChargingLevelDefaultConfig;
-    private int mSmartChargingResumeLevelDefaultConfig;
+    private ISmartCharge mSmartCharge;
+    private int mSmartChargingLevelDefaultConfig = 85;
+    private int mSmartChargingResumeLevelDefaultConfig = 80;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        mSmartChargingLevelDefaultConfig = getResources().getInteger(
-                com.android.internal.R.integer.config_smartChargingBatteryLevel);
-
-        mSmartChargingResumeLevelDefaultConfig = getResources().getInteger(
-                com.android.internal.R.integer.config_smartChargingBatteryResumeLevel);
-
+        mSmartCharge = getSmartCharge();
+        try {
+            mSmartChargingLevelDefaultConfig = mSmartCharge.getSuspendLevel();
+            mSmartChargingResumeLevelDefaultConfig = mSmartCharge.getResumeLevel();
+        } catch (RemoteException ex) { }
         mSmartChargingLevel = (CustomSeekBarPreference) findPreference(KEY_SMART_CHARGING_LEVEL);
         int currentLevel = Settings.System.getInt(getContentResolver(),
             Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
@@ -90,29 +94,49 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        int resumeLevel = Settings.System.getInt(getContentResolver(),
+                 Settings.System.SMART_CHARGING_RESUME_LEVEL, mSmartChargingResumeLevelDefaultConfig);
+        int suspendLevel = Settings.System.getInt(getContentResolver(),
+                 Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
+
         if (preference == mSmartChargingLevel) {
-            int smartChargingLevel = (Integer) objValue;
-            int mChargingResumeLevel = Settings.System.getInt(getContentResolver(),
-                     Settings.System.SMART_CHARGING_RESUME_LEVEL, mSmartChargingResumeLevelDefaultConfig);
+            suspendLevel = (Integer) objValue;
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_LEVEL, smartChargingLevel);
-                mSmartChargingResumeLevel.setMax(smartChargingLevel - 1);
-            if (smartChargingLevel <= mChargingResumeLevel) {
-                mSmartChargingResumeLevel.setValue(smartChargingLevel - 1);
+                    Settings.System.SMART_CHARGING_LEVEL, suspendLevel);
+            mSmartChargingResumeLevel.setMax(suspendLevel - 1);
+            if (suspendLevel <= resumeLevel) {
+                resumeLevel--;
+                mSmartChargingResumeLevel.setValue(suspendLevel -1);
                 Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_RESUME_LEVEL, smartChargingLevel - 1);
+                    Settings.System.SMART_CHARGING_RESUME_LEVEL, resumeLevel);
             }
+            try {
+                mSmartCharge.updateBatteryLevels(suspendLevel, resumeLevel);
+            } catch (RemoteException ex) { }
             return true;
         } else if (preference == mSmartChargingResumeLevel) {
-            int smartChargingResumeLevel = (Integer) objValue;
-            int mChargingLevel = Settings.System.getInt(getContentResolver(),
-                     Settings.System.SMART_CHARGING_LEVEL, mSmartChargingLevelDefaultConfig);
-                mSmartChargingResumeLevel.setMax(mChargingLevel - 1);
-               Settings.System.putInt(getContentResolver(),
-                    Settings.System.SMART_CHARGING_RESUME_LEVEL, smartChargingResumeLevel);
+            resumeLevel = (Integer) objValue;
+            mSmartChargingResumeLevel.setMax(suspendLevel - 1);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.SMART_CHARGING_RESUME_LEVEL, resumeLevel);
+            try {
+                mSmartCharge.updateBatteryLevels(suspendLevel, resumeLevel);
+            } catch (RemoteException ex) { }
             return true;
         } else {
             return false;
         }
+    }
+
+    private synchronized ISmartCharge getSmartCharge() {
+        try {
+            return ISmartCharge.getService();
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchElementException ex) {
+            // service not available
+        }
+
+        return null;
     }
 }