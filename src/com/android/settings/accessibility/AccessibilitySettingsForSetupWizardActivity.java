aff26793973ba9d02f7a0256672c3fbc458cc8d8
Henrique Silva <jhenrique09.mcz@hotmail.com>
2022-05-13 01:58:07 +0000

    Merge "Revert "Settings: Fix NPE when launching accessibility settings from GMS SUW"" into snow

--- src/com/android/settings/accessibility/AccessibilitySettingsForSetupWizardActivity.java
+++ src/com/android/settings/accessibility/AccessibilitySettingsForSetupWizardActivity.java
@@ -106,6 +106,7 @@ public class AccessibilitySettingsForSetupWizardActivity extends SettingsActivit
         super.onCreate(savedState);
         applyTheme();
         tryLaunchFontSizeSettings();
+        mCollapsingToolbarLayout.setVisibility(View.GONE);
         findViewById(R.id.content_parent).setFitsSystemWindows(false);
     }
 
